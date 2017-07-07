c7dcee4fbd0cdaef112da28acbdd3604149594c5
Jakub Tucholski <jakub@shoguninc.com>
2017-07-07 13:31:17 -0700

    rust still broken

--- src/rand_range.rs
+++ src/rand_range.rs
@@ -1,4 +1,4 @@
-extern crate rand;
+#![feature(rand)]
 
 use rand::Rng;
 
@@ -16,4 +16,25 @@ pub mod rand_range {
 fn test_check_vector_sizes() {
     let size_10 = rand_range::generate_random_range(10);
     assert_eq!(size_10.len(), 10);
+
+    let size_million = rand_range::generate_random_range(1000000);
+    assert_eq!(size_million.len(), 1000000);
+}
+
+#[test]
+fn test_ensure_not_equal() {
+    let vec_one = rand_range::generate_random_range(1000000);
+    let vec_two = rand_range::generate_random_range(1000000);
+
+    let mut idx: usize = 0;
+    let mut equal = true;
+
+    while idx < 1000000 {
+        if vec_one[idx] != vec_two[idx] {
+            equal = false;
+        }
+        idx += 1;
+    }
+
+    assert_eq!(equal, false);
 }
